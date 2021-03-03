64e91505534a728e4cfb47427d1dedb4b76e693a
Li Chuangbo <im@chuangbo.li>
2021-03-03 20:01:30 +0800

    fix: allow to check version before update db

--- xip.go
+++ xip.go
@@ -23,11 +23,15 @@ var (
 )
 
 func main() {
-	// can be download from https://github.com/out0fmemory/qqwry.dat
 	flag.StringVar(&dbFile, "db", defaultDbFile, "纯真IP库")
 	v := flag.Bool("v", false, "Print the version number of xip")
 
 	flag.Parse()
+
+	if *v {
+		fmt.Println(version)
+		os.Exit(0)
+	}
 
 	if flag.Arg(0) == "update" {
 		if err := download(dbFile); err != nil {
@@ -36,25 +40,22 @@ func main() {
 		os.Exit(0)
 	}
 
+	isFromPipe := fromPipe()
+
+	if flag.NArg() == 0 && !isFromPipe {
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	db, err := qqwry.Open(dbFile)
 	if err != nil {
 		fmt.Printf("纯真IP库 \"%s\" 不存在，可以使用 xip update 命令下载\n", dbFile)
 		log.Fatal(err)
 	}
 
-	if *v {
-		fmt.Println(version)
-		os.Exit(0)
-	}
-
-	if fromPipe() {
+	if isFromPipe {
 		pipeMode(db)
 		return
-	}
-
-	if flag.NArg() == 0 {
-		flag.Usage()
-		os.Exit(1)
 	}
 
 	cliMode(db, flag.Args())
